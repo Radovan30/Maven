1a53150f204d26f29f164ae374c4d7ab65cc788a
Radovan <55534148+radovan30@users.noreply.github.com>
2024-02-02 23:49:17 +0100

    dopracovan log a cteni ze souboru

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,25 +1,87 @@
 package org.example;
 
-
+import org.apache.poi.ss.usermodel.DataFormatter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
 
 public class Main {
 
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
 
-        logger.info("Ahoj");
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        System.out.println("Zadejte cestu k souboru: ");
+        Scanner scanner = new Scanner(System.in);
+        String pathEntry = scanner.nextLine();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        File path = new File(pathEntry);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        // Kontrola zda existuje soubot - jestli ANO tak se pokracuje v praci / jestli NE tak se program ukonci
+        if(path.exists())
+        {
+            String fileName = path.getName();
+
+            logger.info("Cesta k souboru je spravna, pokracuji ve cteni souboru - " + fileName);
+            System.out.println("Cesta k souboru je spravna, pokracuji ve cteni souboru " + fileName);
+
+            try {
+                // Prace z excelem
+                FileInputStream file = new FileInputStream(path);
+                XSSFWorkbook workbook = new XSSFWorkbook(file);
+                DataFormatter dataFormatter = new DataFormatter();
+
+                // Ziska prvni list v souboru (nazev listu "List1")
+                XSSFSheet sheet = workbook.getSheet("List1");
+
+                // Projde vsechny radky v listu
+                for (Row row : sheet) {
+                    // Ziska hodnoty z konkretniho sloupce (napr. sloupec B/1)
+                    Cell cell = row.getCell(1);
+                    String cellValue = dataFormatter.formatCellValue(cell);
+
+                    // Kontrola typu hodnoty
+                   // if (cell.getCellType() == CellType.NUMERIC) {
+
+                        // Pokud je hodnota cele cislo, vypiseme ji
+                        //int integer = (int) cell.getNumericCellValue();
+
+                    try {
+                        int integer = Integer.parseInt(cellValue);
+                        if (IsPrime.isPrime(integer)) {
+                            String integerString = String.valueOf(integer);
+                            logger.info("Prvocislo - " + integerString);
+                            System.out.println(integer);
+                        }
+                    }
+                    catch (NumberFormatException e) {
+                        // Ignorujeme řádky neobsahující celé číslo
+
+                    }
+                   // }
+                }
+
+                logger.info("Ukonceno cteni souboru, zaviram soubor!");
+                System.out.println("Ukonceno cteni souboru, zaviram soubor!");
+                file.close();
+
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+                logger.info("Chyba pri cteni souboru " + e.getMessage());
+            }
+
+        } else {
+            logger.info("Soubor neexistuje, nebo je spatne zadana cesta k souboru!");
+            System.out.println("Soubor neexistuje, nebo je spatne zadana cesta k souboru!");
         }
+
     }
 }
